2d6138547f831878b77493bdd0bbe4f9839d5404
DeathStare~pog <saurav.gta@gmail.com>
2021-04-02 23:56:44 +0530

    plot 3

---
+++ plot3.R
@@ -0,0 +1,22 @@
+library("data.table")
+setwd("C:/Users/Saurav Mohapatra/Desktop/Coursera/Assignment1")
+pd <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+pd[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+pd[, Date := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+pd <- pd[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot3.png", width=480, height=480)
+
+plot(pd[, Date], pd[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(pd[, Date], pd[, Sub_metering_2],col="red")
+lines(pd[, Date], pd[, Sub_metering_3],col="blue")
+legend("topright"
+       , col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       ,lty=c(1,1), lwd=c(1,1))
+
+dev.off()
+
+
+
